e4279288003136f248278fc5ad3a59a4923debec
Paulo Martins <paulormart@users.noreply.github.com>
2019-04-10 17:55:56 +0100

    index.js
    If allowClick is not active theres no reason for the thumb to jump to the maximum allowed value

--- src/index.js
+++ src/index.js
@@ -77,7 +77,7 @@ class Roundy extends Component {
     // e.preventDefault()
     // we update first value, then we decide based on rotation
     if (!this.isDrag) {
-      this.updateValue(e, true)
+      this.updateValue(e, this.props.allowClick)
     }
     this.allowChange = true
     this.isDrag = true
